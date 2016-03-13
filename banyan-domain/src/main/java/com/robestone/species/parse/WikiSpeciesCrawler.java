433674ecdf947e979fc6f2f279e76be12ad27fa4
jacob <jacob@JacobLaptop2015.jisrfiberwired>
2016-03-12 21:40:52 -0600

    "+Name"

--- banyan-domain/src/main/java/com/robestone/species/parse/WikiSpeciesCrawler.java
+++ banyan-domain/src/main/java/com/robestone/species/parse/WikiSpeciesCrawler.java
@@ -134,6 +134,7 @@ public class WikiSpeciesCrawler extends AbstractWorker {
 		// search for the right patterns, ie <a href="/wiki/Biciliata"
 		Set<String> links = parseLinks(page);
 		for (String link: links) {
+			link = link.trim(); // "+Pirocydonia" is still busting my hump 
 			ParseStatus status = new ParseStatus();
 			status.setUrl(link);
 			status.setStatus(ParseStatus.FOUND);
@@ -162,7 +163,6 @@ public class WikiSpeciesCrawler extends AbstractWorker {
 			// save the links
 			String link = matcher.group(1);
 			link = StringUtils.replace(link, "_", " ");
-			link = EntryUtilities.urlDecode(link);
 			link = WikiSpeciesParser.cleanCharacters(link); 
 			if (!isSkippableLink(link)) {
 				links.add(link);
