3b95f20627de48958c0b557ba8189476435bd2f0
U-USDA\jacob.robertson <jacob.robertson@ASAMOKAN5120094.usda.net>
2018-06-04 14:41:02 -0500

    testing messages

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 JAVA5="/cygdrive/C/Program Files/Java/jdk1.5.0_22"
 JAVA6="/cygdrive/C/Program Files/Java/jdk1.6.0_45"
 JAVA7="/cygdrive/C/Program Files/Java/jdk1.7.0_80"
-JAVA8="/cygdrive/C/Program Files/Java/jdk1.8.0_65"
+JAVA8="/cygdrive/C/softwaredistribution/Sun/jdk1.8.0_45"
 
 MVN2="/cygdrive/C/Program Files (x86)/apache-maven-2.2.1/bin"
 MVN3="/cygdrive/C/Program Files (x86)/apache-maven-3.3.9/bin"
 
-export JAVA_HOME="$JAVA7"
+export JAVA_HOME="$JAVA8"
 export MVN_HOME="$MVN3"
 
 echo JAVA_HOME=$JAVA_HOME
