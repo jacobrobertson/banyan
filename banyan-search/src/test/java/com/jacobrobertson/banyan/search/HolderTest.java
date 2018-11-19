2c6c4ad2e2572ff0d9beb1c957411852d807f6e1
jacob <jacob@JacobLaptop2015>
2018-11-19 10:34:52 -0600

    holder

---
+++ banyan-search/src/test/java/com/jacobrobertson/banyan/search/HolderTest.java
@@ -0,0 +1,14 @@
+package com.jacobrobertson.banyan.search;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class HolderTest extends TestCase {
+
+	@Test
+	public void testThis() {
+		// nothing
+	}
+	
+}
