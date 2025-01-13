0e02c778506e216e8d81b2a1d5113eac20992648
jacob.cs.robertson <jacob.cs.robertson@gmail.com jacob.cs.robertson@gmail.com>
2025-01-13 13:17:31 -0600

    search index

--- banyan-domain/src/main/java/com/robestone/species/js/SearchIndexBuilder.java
+++ banyan-domain/src/main/java/com/robestone/species/js/SearchIndexBuilder.java
@@ -1,7 +1,6 @@
 package com.robestone.species.js;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import com.robestone.species.CompleteEntry;
@@ -17,26 +16,31 @@ public class SearchIndexBuilder extends AbstractWorker {
 	private String letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 	private int minKeyLen = 3;
 	private int maxKeyLen = 4;
-	private List<String> entryNames;
 	
 	private static class KeyEntry {
 		String key;
-		List<String> topMatches = new ArrayList<String>();
+		List<CandidateEntry> topMatches = new ArrayList<CandidateEntry>();
 		List<KeyEntry> children = new ArrayList<KeyEntry>();
+	}
+	public static class CandidateEntry {
+		CompleteEntry entry;
+		List<String> names = new ArrayList<>();
 	}
 
 	public SearchIndexBuilder() {
 	}
 	
 	public void iterateOverKeys() {
-		// init method - only call once
-		entryNames = getNames();
+		List<CandidateEntry> entryNames = getCandidates();
+		iterateOverKeys(entryNames);
+	}
+	public void iterateOverKeys(List<CandidateEntry> candidates) {
 		
 		KeyEntry root = new KeyEntry();
 		root.key = "";
-		iterateOverKeys(root);
+		iterateOverKeys(root, candidates);
 	}
-	private void iterateOverKeys(KeyEntry key) {
+	private void iterateOverKeys(KeyEntry key, List<CandidateEntry> candidates) {
 		if (key.key.length() == maxKeyLen) {
 			return;
 		}
@@ -46,62 +50,79 @@ public class SearchIndexBuilder extends AbstractWorker {
 			// check the min length - no point to visit 1 letter?
 			boolean foundMatches = true;
 			if (nextKey.key.length() >= minKeyLen) {
-				foundMatches = visitKey(nextKey);
+				foundMatches = visitKey(nextKey, candidates);
 			}
 				
 			if (foundMatches) {
 				key.children.add(nextKey);
 				// we only go deeper if we found a match, otherwise there's no point
-				iterateOverKeys(nextKey);
+				iterateOverKeys(nextKey, candidates);
 			}
 		}
 	}
 	
-	private boolean visitKey(KeyEntry key) {
+	private boolean visitKey(KeyEntry key, List<CandidateEntry> candidates) {
 //		System.out.println("visitKey." + key.key);
 		
-		for (String name : entryNames) {
-			if (name.contains(key.key)) {
-				if (key.topMatches.size() <= topMatchesMax) {
-					key.topMatches.add(name);
+		for (CandidateEntry candidate : candidates) {
+			
+			// check all versions of the name
+			
+			// TODO SCORE THESE
+			for (String name : candidate.names) {
+				if (name.contains(key.key)) {
+					if (key.topMatches.size() <= topMatchesMax) {
+						key.topMatches.add(candidate);
+						break;
+					}
 				}
 			}
 		}
 
 		boolean found = !key.topMatches.isEmpty();
 		if (found) {
-			System.out.println("visitKey." + key.key + "." + key.topMatches);
+			System.out.println("visitKey." + key.key + "." + key.topMatches.size());
 		}
 		
 		return found;
 	}
-	
-	private List<String> getNames() {
-		List<String> list = new ArrayList<String>();
+	private List<CandidateEntry> getCandidates() {
+		List<CompleteEntry> entries = speciesService.findEntriesForLuceneIndex();
+		return getCandidates(entries);
+	}
+	public CandidateEntry toCandidate(CompleteEntry entry) {
+		CandidateEntry candidate = new CandidateEntry();
+		if (entry.getLatinNameCleanest() != null) {
+			candidate.names.add(entry.getLatinNameCleanest());
+		} else if (entry.getLatinName() != null) {
+			candidate.names.add(entry.getLatinName().toUpperCase());
+		}
 		
-		Collection<String> latinNames = speciesService.findAllLatinNames();
-		for (String latin : latinNames) {
-			CompleteEntry entry = speciesService.findEntryByLatinName(latin);
-			if (entry != null) {
+		if (entry.getCommonNameCleanest() != null) {
+			candidate.names.add(entry.getCommonNameCleanest());
+		} else if (entry.getCommonName() != null) {
+			candidate.names.add(entry.getCommonName().toUpperCase());
+		}
 
-				if (entry.getLatinNameCleanest() != null) {
-					list.add(entry.getLatinNameCleanest());
-				} else if (entry.getLatinName() != null) {
-					list.add(entry.getLatinName().toUpperCase());
-				}
-				
-				if (entry.getCommonNameCleanest() != null) {
-					list.add(entry.getCommonNameCleanest());
-				} else if (entry.getCommonName() != null) {
-					list.add(entry.getCommonName().toUpperCase());
-				}
-				if (list.size() % 1000 == 0) {
-					System.out.println("getNames." + list.size());
-				}
+		return candidate;
+	}
+	public List<CandidateEntry> getCandidates(List<CompleteEntry> entries) {
+		List<CandidateEntry> candidates = new ArrayList<CandidateEntry>();
+		
+		// convert each one
+		for (CompleteEntry entry : entries) {
+			CandidateEntry candidate = toCandidate(entry);
+			if (candidate != null) {
+				candidates.add(candidate);
+			}
+			
+			if (candidates.size() % 1000 == 0) {
+				System.out.println("getCandidates." + candidates.size());
 			}
 		}
 		
-		return list;
+		return candidates;
 	}
+	
 	
 }
